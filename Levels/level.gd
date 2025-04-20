54c143f32530f66ef2ffe4cd5dddd9de1d24df64
Hunter <warsvitaly@gmail.com>
2025-04-20 13:51:59 +0800

    init

--- Levels/level.gd
+++ Levels/level.gd
@@ -5,6 +5,7 @@ var box_scene = preload("res://box.tscn")
 func _ready():
 	# Для теста: спавним box при старте уровня
 	spawn_box(Vector3(7.116, 1.715, 0))
+	_on_timer_timeout()
 
 func spawn_box(spawn_position: Vector3):
 	if box_scene:
@@ -13,3 +14,9 @@ func spawn_box(spawn_position: Vector3):
 		add_child(box_instance)
 	else:
 		print("Ошибка: box_scene не назначена!")
+
+func _on_timer_timeout() -> void:
+	var x = randf_range(-10.0, 10.0)   # Замените диапазон на свой
+	var y =  0.5                     # Например, высота над землёй
+	var z = randf_range(-10.0, 10.0)   # Замените диапазон на свой
+	spawn_box(Vector3(x,y,z))
