4567704c7d61338920a955e6264df9b6194f01ae
Hunter <warsvitaly@gmail.com>
2025-04-21 22:31:17 +0800

    update

--- Levels/level.gd
+++ Levels/level.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
-var box_scene = preload("res://box.tscn")
+var box_scene = preload("res://Assets/Platform/box.tscn")
 
 @onready var player = $Player
+@onready var scoreText = $UI/ScoreText
 
 func _ready():
 	spawn_box(Vector3(7.116, 1.715, 0))
@@ -19,13 +20,32 @@ func spawn_box(spawn_position: Vector3):
 		print("Ошибка: box_scene не назначена!")
 
 func _on_box_destroyed():
-	player.score += 1
-	print("Счёт: %s" % player.score)
+	player._add_score()
+	# print("Счёт: %s" % player.score)
 	# Если есть Label для счёта:
-	$UserInterface/ScoreText.text = "Score: %s" % player.score
+	scoreText.text = "Score: %s" % player.score
 
 func _on_timer_timeout() -> void:
 	var x = randf_range(-10.0, 10.0)
 	var y = 1.2
 	var z = randf_range(-10.0, 10.0)
 	spawn_box(Vector3(x, y, z))
+	
+func restart_level():
+	# Перезагрузка сцены
+	get_tree().reload_current_scene()
+
+func _input(event):
+	if event.is_action_pressed("restart"):
+		restart_level()
+
+func _on_teleport_body_entered(body: Node3D) -> void:
+	var spawn_points = [$Spawn1, $Spawn2, $Spawn3]  # Массив точек спауна
+  
+	if body.name == "Player":
+		# Выбираем случайную точку из массива
+		var random_spawn = spawn_points[randi() % spawn_points.size()]
+	  
+		# Телепортируем игрока с небольшим смещением вверх (чтобы не застрять в полу)
+		body.global_transform.origin = random_spawn.global_transform.origin + Vector3(0, 0.5, 0)
+	
